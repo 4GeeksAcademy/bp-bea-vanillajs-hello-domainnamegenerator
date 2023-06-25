be05966b7954aff373e2a3a7d6fd4d5c792875cf
bea-bp <129992845+bea-bp@users.noreply.github.com>
2023-06-25 15:18:00 +0000

    domain genarator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronouns = ["the", "our"];
+const adjs = ["great", "big", "awesome"];
+const nouns = ["jogger", "racoon"];
+const dotComs = ["com", "net", "org"];
+
+const domainNameGeneratorFors = (pronouns, adjs, nouns, dotComs) => {
+  let generatedDomains = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjs.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < dotComs.length; l++) {
+          generatedDomains.push(
+            `${pronouns[i]}${adjs[j]}${nouns[k]}.${dotComs[l]}`
+          );
+        }
+      }
+    }
+  }
+
+  return generatedDomains;
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.printDomains = function() {
+    const domains = domainNameGeneratorFors(pronouns, adjs, nouns, dotComs);
+    const domainElement = document.getElementById("domainList");
+    domainElement.innerHTML = "";
+    domains.forEach(domain => {
+      domainElement.innerHTML += `<p>${domain}</p>`;
+    });
+  };
 };
